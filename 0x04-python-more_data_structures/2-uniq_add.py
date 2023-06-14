b00a4b9ef31f7175c9402268903bf5d7449f0dba
Tash-01 <tashfernandes93@gmail.com>
2023-06-14 09:19:54 -0700

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/puthon3
 def uniq_add(my_list=[]):
-    return sum(set(my_list))
+    my_sum = sum(my_list)
+    len = 0
+    for number in my_sum:
+        len += number
+        return (len)
