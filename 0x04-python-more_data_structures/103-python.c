1aadadccd1b21684fd1ba3d2d65b355b901f135c
Tash-01 <tashfernandes93@gmail.com>
2023-06-14 09:29:46 -0700

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -47,5 +47,19 @@ void print_python_list(PyObject *p)
 	PyListObject *list;
 	PyObject *obj;
 
+	size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
 
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		obj = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		if (PyBytes_Check(obj))
+			print_python_bytes(obj);
+	}
+}
 
