a632a92255503ef0d46ba08cc0fc2f201484800f
Tash-01 <tashfernandes93@gmail.com>
2023-06-14 08:14:57 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is none or type(roman_string) != str:
+        return 0
+    val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numberss = [val[c] for c in roman_string] + [0]
+    res  = 0
 
+    for x in range(len(numbers) - 1):
+        if numbers[x] >= numbers[x + 1]:
+            res += numbers[x]
+        else:
+            res -= numbers[x]
+            return (res)
